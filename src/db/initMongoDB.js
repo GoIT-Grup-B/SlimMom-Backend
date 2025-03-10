e2d9f55190dc5fe4da2a086d867def9a18959d3d
gizemcoban <gizem.coban.r@gmail.com>
2025-03-10 20:05:48 +0100

    init mongodb

--- src/db/initMongoDB.js
+++ src/db/initMongoDB.js
@@ -1,5 +1,3 @@
-// src/db/initMongoDB.js
-
 import mongoose from 'mongoose';
 
 import { env } from '../utils/env.js';
@@ -9,14 +7,13 @@ export const initMongoDB = async () => {
     const user = env('MONGODB_USER');
     const pwd = env('MONGODB_PASSWORD');
     const url = env('MONGODB_URL');
-    const db = env('MONGODB_DB');
 
     await mongoose.connect(
-      `mongodb+srv://${user}:${pwd}@${url}/${db}?retryWrites=true&w=majority`,
+      mongodb+srv://${user}:${pwd}${url}/slim-mom?retryWrites=true&w=majority,
     );
     console.log('Mongo connection successfully established!');
   } catch (e) {
     console.log('Error while setting up mongo connection', e);
-    throw e;
-  }
+    throw e;
+  }
 };
