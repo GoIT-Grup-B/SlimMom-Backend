835f34619e2d6da3a1ca1d7b5d07b60bd79c2ca6
gizemcoban <gizem.coban.r@gmail.com>
2025-03-10 21:57:56 +0100

    register

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -1,10 +1,26 @@
-import { registerUser } from "../services/auth"
-export const registerUserController = async(req, res, next)=>{
-    const user = await registerUser(req.body);
+import {registerUser,getUser} from '../services/auth.js';
+import createHttpError from "http-errors";
 
-    res.status(200).json({
-        status:200,
-        message:'Successfully registered a user!',
-        data: user
-    })
-}
+export const registerUserController = async(req, res, next)=>{
+    try{
+        const {name, email, password} = req.body;
+        const user = await getUser(email);
+        console.log(user);
+        if(user){
+            return next(createHttpError(409,'Email in use'));
+        }
+
+        const newUser = await registerUser({name, email, password});
+        const userwithoutpass = {...newUser};
+        delete userwithoutpass.password;
+
+        res.status(201).json({
+            status:201,
+            message: "Successfully registered a user!",
+            data: userwithoutpass
+        });
+        
+    }catch(error){
+        next(createHttpError(500,error));
+    }
+};
