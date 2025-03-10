1567d84c18291aef161dac4993fe32f8e7d3f181
Murat Çolak <mo939614@gmail.com>
2025-03-11 00:10:39 +0300

    Editing for array format

--- src/db/models/products.model.js
+++ src/db/models/products.model.js
@@ -1,24 +1,19 @@
 const { Schema, model } = require('mongoose');
+
 const productSchema = new Schema({
-  categories: [{ type: String }],
+  categories: { type: String },
   weight: {
     type: Number,
     required: true,
   },
-  title: {
-    ru: { type: String, required: true },
-    ua: { type: String, required: true },
-  },
+  title: { type: String, required: true },
   calories: {
     type: Number,
     required: true,
   },
-  groupBloodNotAllowed: {
-    1: { type: Boolean, required: true },
-    2: { type: Boolean, required: true },
-    3: { type: Boolean, required: true },
-    4: { type: Boolean, required: true },
-  },
+  groupBloodNotAllowed: [{ type: Boolean, required: true }],
 });
+
 const Product = model('product', productSchema);
+
 module.exports = { Product };
