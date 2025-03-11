a60432bc135782d45121efc1d0b4f0af0f6141bc
Heza GÖLCÜK <h.golcuk91@outlook.com>
2025-03-11 22:04:34 +0300

    login finished

--- src/routers/auth.js
+++ src/routers/auth.js
@@ -1,20 +1,24 @@
 import { Router } from 'express';
 import ctrlWrapper from '../utils/ctrlWrapper.js';
 import validateBody from '../middlewares/validateBody.js';
-import { registerUserSchema } from '../validation/auth.js';
+import { registerUserSchema, loginUserSchema } from '../validation/auth.js';
 import {
   registerUserController,
   loginUserController,
-  refreshUserController,
 } from '../controllers/auth.js';
-import { loginUserSchema } from '../validation/auth.js';
 
 const router = Router();
 
 router.post(
-  '/auth/register',
+  '/register',
   validateBody(registerUserSchema),
   ctrlWrapper(registerUserController),
+);
+
+router.post(
+  '/login',
+  validateBody(loginUserSchema),
+  ctrlWrapper(loginUserController),
 );
 
 export default router;
