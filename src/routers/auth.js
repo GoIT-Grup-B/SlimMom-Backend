add5d1daadd98b58d3c03984f3ff6720b45af7cd
subat <subat.yucel1@gmail.com>
2025-03-11 16:40:45 +0300

    get daily rate route is implemented & routes are fixed to collect them in one place

--- src/routers/auth.js
+++ src/routers/auth.js
@@ -4,10 +4,12 @@ import validateBody from '../middlewares/validateBody.js';
 import { registerUserSchema } from '../validation/auth.js';
 import { registerUserController } from '../controllers/auth.js';
 
-
 const router = Router();
 
-router.post('/auth/register', validateBody(registerUserSchema), ctrlWrapper(registerUserController));
-
+router.post(
+  '/register',
+  validateBody(registerUserSchema),
+  ctrlWrapper(registerUserController),
+);
 
 export default router;
---
+++ src/routers/user.js
@@ -0,0 +1,14 @@
+import { Router } from 'express';
+import { getDailyRateController } from '../controllers/user.js';
+import validateBody from '../middlewares/validateBody.js';
+import { getDailyRateSchema } from '../validation/user.js';
+
+const router = Router();
+
+router.get(
+  '/daily-rate',
+  validateBody(getDailyRateSchema),
+  getDailyRateController,
+);
+
+export default router;
