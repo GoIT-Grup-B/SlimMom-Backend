feca5838f31b78168813e866c5ba47454c01c9ec
rojhatbalta <rojhatbalta@gmail.com>
2025-03-12 22:29:35 +0300

    update userinfo

--- src/db/models/users.js
+++ src/db/models/users.js
@@ -1,33 +1,66 @@
-import { Schema, model } from "mongoose";
+import { Schema, model } from 'mongoose';
 
-const userSchema = new Schema({
-    name:{
-        type: String,
-        required: true,
+const userSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
     },
-    email:{
-        type: String,
-        required: true,
-        unique: true,
+    email: {
+      type: String,
+      required: true,
+      unique: true,
     },
-    password:{
-        type: String,
-        required: true,
-    }
-},
-{
+    password: {
+      type: String,
+      required: true,
+    },
+    infouser: {
+      currentWeight: {
+        type: Number,
+        default: null,
+      },
+      height: {
+        type: Number,
+        default: null,
+      },
+      age: {
+        type: Number,
+        default: null,
+      },
+      desireWeight: {
+        type: Number,
+        default: null,
+      },
+      bloodType: {
+        type: Number,
+        default: null,
+      },
+      dailyRate: {
+        type: Number,
+        default: null,
+      },
+      notAllowedProducts: {
+        type: [String],
+        default: null,
+      },
+      notAllowedProductsAll: {
+        type: [String],
+        default: null,
+      },
+    },
+  },
+  {
     timestamps: true,
-    versionKey: false
-}
-
+    versionKey: false,
+  },
 );
 
-
 userSchema.methods.toJSON = function () {
-    const obj = this.toObject();
-    delete obj.password;
-    return obj;
-  };
+  const obj = this.toObject();
+  delete obj.password;
+  return obj;
+};
 
 export const UserCollection = model('users', userSchema);
 
